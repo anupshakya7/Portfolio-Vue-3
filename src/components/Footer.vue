40e2b765f69920de0ea140864c60d3cb875f6b0e
Anup Shakya <dev2@krizmatic.com>
2025-01-16 11:57:13 +0545

    Add Copyright Symbol and Google Verification Code in Site

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@
             <a href="#"><img src="/public/assets/github.svg" class="h-8 bg-gray-900 p-2 cursor-pointer rounded-full transition ease-in-out duration-100 hover:scale-[1.1]" alt=""></a>
             <a href="#"><img src="/public/assets/twitter.svg" class="h-8 bg-gray-900 p-2 cursor-pointer rounded-full transition ease-in-out duration-100 hover:scale-[1.1]" alt=""></a>
         </div>
-        <p class="text-gray-500">{{currentYear}} Anup All Right Reserved</p>
+        <p class="text-gray-500">&copy; {{currentYear}} Anup. All Rights Reserved.</p>
     </footer>
 </template>
 
